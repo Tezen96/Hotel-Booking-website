0356bfb751ecc91bd2e6f360844fb6c60cfdb0c5
Tezen96 <96522837+Tezen96@users.noreply.github.com>
2024-08-06 09:36:35 +0545

    Update config.php

--- templates/config.php
+++ templates/config.php
@@ -1,18 +1,17 @@
-<?php 
+<?php
 
-
-$servername = "db";
+$servername = "db"; // Use the service name defined in docker-compose.yml
 $username = "root";
 $password = "";
 $db_name = "hotel";
 
-$conn = mysqli_connect( $servername, $username, $password, $db_name );
+$conn = mysqli_connect($servername, $username, $password, $db_name);
 
-if( $conn ) {
-    return $conn;
+if ($conn) {
+    // Connection successful
 } else {
-    echo "Connection is failed!!!";
+    echo "Connection failed: " . mysqli_connect_error(); // Added detailed error message
 }
 
-
 ?>
+
